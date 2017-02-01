0465d18a4321cf36d6a9b7c6b377cc38e16da062
Matildha Sjöstedt <tildhanor@gmail.com>
2017-02-01 15:01:30 +0100

    Syscall create file seems to work with basic test

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -4,6 +4,8 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "filesys/filesys.h"
+#include "filesys/off_t.h"
 
 
 void halt(void);
@@ -28,6 +30,11 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
+  printf("try to create file \n");
+  char filename[5] = {'a','.','t','x','t'};
+  bool success = create(filename, 100);
+  if(success) printf("Succesfully created file a.txt\n");
+
   halt();
   thread_exit ();
 }
@@ -37,11 +44,14 @@ void halt(void)
   power_off();  // Works! (i think)
 }
 
-/*
+
 bool create (const char *file, unsigned initial_size)
 {
+  off_t init_size = (off_t) initial_size;
+  return filesys_create(file, init_size);
 }
 
+/*
 int open (const char *file)
 {
 } 
